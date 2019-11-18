90a26618209f91d4b4b5eda7c38172c76f702b32
BriceB <brice.beteille@gmail.com>
2019-11-18 17:07:05 +0100

    start spring_mvc project, imported from another github rep to have a clean base to work on

--- spring_jdbc/src/main/java/briceb/spring_jdbc/model/Employee.java
+++ spring_jdbc/src/main/java/briceb/spring_jdbc/model/Employee.java
@@ -2,16 +2,8 @@ package briceb.spring_jdbc.model;
 
 import java.sql.Timestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class Employee {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstname;
 	private Timestamp timestamp;
